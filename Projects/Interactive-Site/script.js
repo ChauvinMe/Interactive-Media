1da77066e5b99976f546731dd6fb026009e58906
ChauvinMe <chauvin.me@gmail.com>
2021-06-28 10:15:10 -0400

    Added interactive site and process work

---
+++ Projects/Interactive-Site/script.js
@@ -0,0 +1,93 @@
+// script for draggable elements
+
+var container = document.querySelector("#container");
+var activeItem = null;
+
+var active = false;
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+function dragStart(e) {
+
+  if (e.target !== e.currentTarget) {
+    active = true;
+
+    activeItem = e.target;
+
+    if (activeItem !== null) {
+      if (!activeItem.xOffset) {
+        activeItem.xOffset = 0;
+      }
+
+      if (!activeItem.yOffset) {
+        activeItem.yOffset = 0;
+      }
+
+      if (e.type === "touchstart") {
+        activeItem.initialX = e.touches[0].clientX - activeItem.xOffset;
+        activeItem.initialY = e.touches[0].clientY - activeItem.yOffset;
+        } else {
+        console.log("doing something!");
+        activeItem.initialX = e.clientX - activeItem.xOffset;
+        activeItem.initialY = e.clientY - activeItem.yOffset;
+        }
+      }
+  }
+}
+
+function dragEnd(e) {
+  if (activeItem !== null) {
+    activeItem.initialX = activeItem.currentX;
+    activeItem.initialY = activeItem.currentY;
+  }
+
+  active = false;
+  activeItem = null;
+}
+
+function drag(e) {
+  if (active) {
+    if (e.type === "touchmove") {
+      e.preventDefault();
+
+      activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
+      activeItem.currentY = e.touches[0].clientY - activeItem.initialY;
+      } else {
+      activeItem.currentX = e.clientX - activeItem.initialX;
+      activeItem.currentY = e.clientY - activeItem.initialY;
+      }
+
+      activeItem.xOffset = activeItem.currentX;
+      activeItem.yOffset = activeItem.currentY;
+
+      setTranslate(activeItem.currentX, activeItem.currentY, activeItem);
+    }
+  }
+
+  function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  }
+
+
+
+// script for the video element
+
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+
+
+}
